9df4afb13ff978fbc54b02bd852246a98015bba8
silently <gdenispro@gmail.com>
2016-12-12 10:08:36 +0100

    added age and height to people

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const ready = function() {
   // Loading
   const people = [
-    { firstname: "Landry", surname: "O'Hara" },
-    { firstname: "Sharon", surname: "O'Neil" },
-    { firstname: "Emerson", surname: "O'Connell" },
-    { firstname: "Roberto", surname: "O'Maley" }
+    { firstname: "Landry", surname: "O'Hara", age: 34, height: 175 },
+    { firstname: "Sharon", surname: "O'Neil", age: 51, height: 177 },
+    { firstname: "Emerson", surname: "O'Connell", age: 16, height: 165 },
+    { firstname: "Roberto", surname: "O'Maley", age: 30, height: 199 }
   ];
   //
   const tbody = document.querySelector("tbody");
@@ -14,7 +14,7 @@ const ready = function() {
 
   for(let i = 0; i < people.length; i++) {
     let tr = document.createElement("tr");
-    tr.innerHTML = `<td>${people[i].surname}</td><td>${people[i].firstname}</td>`;
+    tr.innerHTML = `<td>${people[i].surname}</td><td>${people[i].firstname}</td><td>${people[i].age}</td><td>${people[i].height}</td>`;
       trContainer.appendChild(tr);
   }
 
