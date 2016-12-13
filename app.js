d5ed3cfc2a5065f3ac6073c4d5816997e8a6ae6c
silently <gdenispro@gmail.com>
2016-12-13 10:02:58 +0100

    moved to MVC code organization

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
 // app global variable to maintain state
-const app = {};
+const app = {
+  models: {},
+  views: {},
+  controllers: {}
+};
 
-// app model
-app.people = [{
-  firstname: "Landry",
-  surname: "O'Hara",
-  age: 34,
-  height: 175
-}, {
-  firstname: "Sharon",
-  surname: "O'Neil",
-  age: 51,
-  height: 177
-}, {
-  firstname: "Emerson",
-  surname: "O'Connell",
-  age: 16,
-  height: 165
-}, {
-  firstname: "Roberto",
-  surname: "O'Maley",
-  age: 30,
-  height: 199
-}];
+// app people model
+app.models.people = {
+  get: function() {
+    return this.data;
+  },
+  add: function(newPerson) {
+    this.data.push(newPerson);
+    app.views.redraw();
+  },
+  // fake data to initialize example
+  data: [{
+    firstname: "Landry",
+    surname: "O'Hara",
+    age: 34,
+    height: 175
+  }]
+};
 
-// app controller
-app.redraw = function() {
+// app views
+app.views.redraw = function() {
+  // TODO : sauver app.people dans le localStorage
+  const people = app.models.people.get();
   const tbody = document.querySelector("tbody");
+  const trContainer = document.createDocumentFragment();
+
   // Remove tbody contents
   while (tbody.firstChild) {
     tbody.removeChild(tbody.firstChild);
   }
-  // Add new contents from app.people
-  const trContainer = document.createDocumentFragment();
-  for (let i = 0; i < app.people.length; i++) {
+  // Add new contents from people
+  for (let i = 0; i < people.length; i++) {
     let tr = document.createElement("tr");
-    tr.innerHTML = `<td>${app.people[i].surname}</td><td>${app.people[i].firstname}</td><td>${app.people[i].age}</td><td>${app.people[i].height}</td>`;
+    tr.innerHTML = `<td>${people[i].surname}</td><td>${people[i].firstname}</td><td>${people[i].age}</td><td>${people[i].height}</td>`;
     trContainer.appendChild(tr);
   }
   tbody.appendChild(trContainer);
 };
 
-// app view
-app.onSubmit = function(e) {
+// app controllers
+app.controllers.onSubmit = function(e) {
   e.preventDefault();
   const formData = new FormData(this);
   const newPerson = {
@@ -51,15 +53,15 @@ app.onSubmit = function(e) {
     age: formData.get("age"),
     height: formData.get("height")
   };
-  app.people.push(newPerson);
-  app.redraw();
+  app.models.people.add(newPerson);
 };
 
 // DOM bindings
 const ready = function() {
+  // TODO charger app.people depuis le localStorage
   // Initial drawing
-  app.redraw();
+  app.views.redraw();
   // On form submit
-  document.getElementById("add-person").addEventListener("submit", app.onSubmit);
+  document.getElementById("add-person").addEventListener("submit", app.controllers.onSubmit);
 }
 document.addEventListener("DOMContentLoaded", ready);
